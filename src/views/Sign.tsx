06a8a501b4541317b488e14e103904f86f7341c1
vgsnv <vgsnv@yandex.ru>
2017-12-17 21:05:19 +0300

    Добавляет немного стилей

--- src/views/Sign.tsx
+++ src/views/Sign.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import * as css from './styles.less';
 
 import Sign from 'components/Sign';
 
 export default () => (
-  <main>
+  <main
+    className={css.sign}
+  >
     <Sign />
   </main>
 );
