a6c3f313a8a9aa9270240450314f18eddbfcbfcd
vgsnv <vgsnv@yandex.ru>
2018-07-06 20:05:48 +0300

    Меняет getBudgets -> getbudgets

--- src/api/app/getbudgets.ts
+++ src/api/app/getbudgets.ts
@@ -4,11 +4,12 @@ const timeout = ms =>
   new Promise((res, rej) => setTimeout(() => rej(new Error("timeout")), ms));
 
 export const query = async () => {
-  return await fetch(`/dzenapi/getBudgets`, {
+  return await fetch(`/dzenapi/getbudgets`, {
     method: "GET",
     credentials: "same-origin",
     headers: HEADERS
   }).then(res => {
+    console.log("res", res);
     if (res.ok) return res.json();
     throw new Error("userinfo failed");
   });
